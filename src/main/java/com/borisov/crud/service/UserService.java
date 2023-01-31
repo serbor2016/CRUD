001571c1ee0761bb3810eb14b18fe7e83a01ae25
handsomesergio <79226336+handsomesergio@users.noreply.github.com>
2023-01-31 21:16:03 +0200

    Add files via upload

---
+++ src/main/java/com/borisov/crud/service/UserService.java
@@ -0,0 +1,36 @@
+package com.borisov.crud.service;
+
+import com.borisov.crud.model.User;
+import com.borisov.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
